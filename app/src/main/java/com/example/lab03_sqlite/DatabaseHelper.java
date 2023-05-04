cad15a8d0b4bd7d98d86509255064ddb82fa8a3a
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2023-05-04 18:02:37 +0700

    add layout

---
+++ app/src/main/java/com/example/lab03_sqlite/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.example.lab03_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_CREATE = "create table Person (_id integer primary key autoincrement, "
+            + "name text not null," +
+            "phone text not null);";
+
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(DATABASE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int NewVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+
+    }
+}
---
+++ app/src/main/java/com/example/lab03_sqlite/DbAdapter.java
@@ -0,0 +1,52 @@
+package com.example.lab03_sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.lab03_sqlite.*;
+
+public class DbAdapter {
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE ="phone";
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase sqLiteDatabase;
+    private static final String DATABASE_NAME = "Database_Demo";
+    private static final String DATABASE_TABLE = "Person";
+    private static final int DATABASE_VERSION = 2;
+    private final Context context;
+
+    public DbAdapter(Context ctx) {
+        this.context = ctx;
+    }
+
+    public DbAdapter open() {
+        dbHelper = new DatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long createUser(String name, String phone) {
+        ContentValues inititalValues = new ContentValues();
+        inititalValues.put(KEY_NAME, name);
+        inititalValues.put(KEY_PHONE, phone);
+        return sqLiteDatabase.insert(DATABASE_TABLE, null, inititalValues);
+    }
+
+    public Cursor getAllPerson() {
+        return sqLiteDatabase.query(DATABASE_TABLE, new String[]{KEY_ID, KEY_NAME, KEY_PHONE}, null, null, null, null, null);
+    }
+
+
+
+
+
+
+}
